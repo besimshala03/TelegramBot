ee5bca5599a6c2411d33ef588bffdd66cc1a2b8d
Besim Shala <besim.shala@bmw.de>
2024-10-19 13:53:28 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MyTelegramBot());
+        } catch (TelegramApiException e) {
+            logger.info(String.valueOf(e));
+        }
+    }
+}
